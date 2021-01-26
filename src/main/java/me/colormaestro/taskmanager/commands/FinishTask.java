d435376797ebbe58ae94d3d1c211f6a26a1e014b
Marek Kadlečík <xkadlec5@fi.muni.cz>
2021-01-26 23:00:54 +0100

    Put database operations on new thread in FinishTask command

--- src/main/java/me/colormaestro/taskmanager/commands/FinishTask.java
+++ src/main/java/me/colormaestro/taskmanager/commands/FinishTask.java
@@ -3,13 +3,16 @@ package me.colormaestro.taskmanager.commands;
 import me.colormaestro.taskmanager.data.DataAccessException;
 import me.colormaestro.taskmanager.data.PlayerDAO;
 import me.colormaestro.taskmanager.data.TaskDAO;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class FinishTask implements CommandExecutor {
     private final TaskDAO taskDAO;
@@ -32,16 +35,22 @@ public class FinishTask implements CommandExecutor {
             return true;
         }
 
+        Plugin plugin = Bukkit.getPluginManager().getPlugin("TaskManager");
         Player p = (Player) sender;
-        try {
-            int assignee = playerDAO.getPlayerID(p.getUniqueId());
-            int id = Integer.parseInt(args[0]);
-            taskDAO.finishTask(id, assignee);
-            p.sendMessage(ChatColor.GREEN + "Task finished.");
-        } catch (SQLException | DataAccessException | NumberFormatException ex) {
-            p.sendMessage(ChatColor.RED + ex.getMessage());
-            ex.printStackTrace();
-        }
+        UUID uuid = p.getUniqueId();
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            try {
+                int assignee = playerDAO.getPlayerID(uuid);
+                int id = Integer.parseInt(args[0]);
+                taskDAO.finishTask(id, assignee);
+                Bukkit.getScheduler().runTask(plugin,
+                        () -> p.sendMessage(ChatColor.GREEN + "Task finished."));
+            } catch (SQLException | DataAccessException | NumberFormatException ex) {
+                Bukkit.getScheduler().runTask(plugin,
+                        () -> p.sendMessage(ChatColor.RED + ex.getMessage()));
+                ex.printStackTrace();
+            }
+        });
         return true;
     }
 }
