6033d088f6d1cb617a41f5f097971ef021a38269
Marek Kadlečík <xkadlec5@fi.muni.cz>
2023-08-02 17:55:42 +0200

    Extracted duplicated code to separate method

--- src/main/java/me/colormaestro/taskmanager/commands/Dashboard.java
+++ src/main/java/me/colormaestro/taskmanager/commands/Dashboard.java
@@ -1,30 +1,19 @@
 package me.colormaestro.taskmanager.commands;
 
 import me.colormaestro.taskmanager.data.TaskDAO;
-import me.colormaestro.taskmanager.model.MemberTaskStats;
-import me.colormaestro.taskmanager.utils.Directives;
-import me.colormaestro.taskmanager.utils.ItemStackBuilder;
+import me.colormaestro.taskmanager.listeners.ClickEventRunnables;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
-
-import java.sql.SQLException;
-import java.util.List;
 
 public class Dashboard implements CommandExecutor {
     private final Plugin plugin;
     private final TaskDAO taskDAO;
-    private static final int INVENTORY_SIZE = 54;
-    private static final int SHOW_SUPERVISED_TASKS_POSITION = 49;
 
     public Dashboard(Plugin plugin, TaskDAO taskDAO) {
         this.plugin = plugin;
@@ -39,43 +28,7 @@ public class Dashboard implements CommandExecutor {
             return true;
         }
 
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            try {
-                List<MemberTaskStats> stats = taskDAO.fetchTaskStatistics();
-                int totalPages = stats.size() / (INVENTORY_SIZE - 9) + 1;
-                // Variable used in lambda should be final or effectively final
-                List<MemberTaskStats> finalStats = stats.stream().limit(INVENTORY_SIZE - 9).toList();
-                Bukkit.getScheduler().runTask(plugin,
-                        () -> {
-                            String inventoryTitle = ChatColor.BLUE + "" + ChatColor.BOLD + "Tasks Dashboard" + ChatColor.RESET + " (1/" + totalPages + ") " + Directives.DASHBOARD;
-                            Inventory inventory = Bukkit.createInventory(player, INVENTORY_SIZE, inventoryTitle);
-
-                            ItemStack stack;
-                            int position = 0;
-                            for (MemberTaskStats memberStats : finalStats) {
-                                stack = ItemStackBuilder.buildMemberStack(memberStats.uuid(), memberStats.ign(), memberStats.doing(), memberStats.finished(), memberStats.approved());
-                                inventory.setItem(position, stack);
-                                position++;
-                            }
-
-                            ItemStackBuilder.supplyInventoryWithPaginationArrows(inventory);
-
-                            stack = new ItemStack(Material.ENDER_EYE, 1);
-                            ItemMeta meta = stack.getItemMeta();
-                            assert meta != null;
-                            meta.setDisplayName(ChatColor.DARK_PURPLE + "Show supervised tasks");
-                            stack.setItemMeta(meta);
-                            inventory.setItem(SHOW_SUPERVISED_TASKS_POSITION, stack);
-
-                            player.openInventory(inventory);
-                        });
-            } catch (SQLException ex) {
-                Bukkit.getScheduler().runTask(plugin,
-                        () -> player.sendMessage(ChatColor.RED + ex.getMessage()));
-                ex.printStackTrace();
-            }
-
-        });
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, ClickEventRunnables.showDashboardView(plugin, taskDAO, player));
 
         return true;
     }
--- src/main/java/me/colormaestro/taskmanager/listeners/SupervisedTasksViewListener.java
+++ src/main/java/me/colormaestro/taskmanager/listeners/SupervisedTasksViewListener.java
@@ -2,31 +2,23 @@ package me.colormaestro.taskmanager.listeners;
 
 import me.colormaestro.taskmanager.data.DataAccessException;
 import me.colormaestro.taskmanager.data.TaskDAO;
-import me.colormaestro.taskmanager.model.MemberTaskStats;
 import me.colormaestro.taskmanager.model.Task;
 import me.colormaestro.taskmanager.utils.Directives;
-import me.colormaestro.taskmanager.utils.ItemStackBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
 import java.sql.SQLException;
-import java.util.List;
 
 public class SupervisedTasksViewListener implements Listener {
     private final Plugin plugin;
     private final TaskDAO taskDAO;
-    private static final int INVENTORY_SIZE = 54;
-    private static final int SHOW_SUPERVISED_TASKS_POSITION = 49;
 
     public SupervisedTasksViewListener(Plugin plugin, TaskDAO taskDAO) {
         this.plugin = plugin;
@@ -77,42 +69,7 @@ public class SupervisedTasksViewListener implements Listener {
     }
 
     private void handleSpectralArrowClick(HumanEntity player) {
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            try {
-                List<MemberTaskStats> stats = taskDAO.fetchTaskStatistics();
-                int totalPages = stats.size() / (INVENTORY_SIZE - 9) + 1;
-                // Variable used in lambda should be final or effectively final
-                List<MemberTaskStats> finalStats = stats.stream().limit(INVENTORY_SIZE - 9).toList();
-                Bukkit.getScheduler().runTask(plugin,
-                        () -> {
-                            String inventoryTitle = ChatColor.BLUE + "" + ChatColor.BOLD + "Tasks Dashboard" + ChatColor.RESET + " (1/" + totalPages + ") " + Directives.DASHBOARD;
-                            Inventory inventory = Bukkit.createInventory(player, INVENTORY_SIZE, inventoryTitle);
-
-                            ItemStack stack;
-                            int position = 0;
-                            for (MemberTaskStats memberStats : finalStats) {
-                                stack = ItemStackBuilder.buildMemberStack(memberStats.uuid(), memberStats.ign(), memberStats.doing(), memberStats.finished(), memberStats.approved());
-                                inventory.setItem(position, stack);
-                                position++;
-                            }
-
-                            ItemStackBuilder.supplyInventoryWithPaginationArrows(inventory);
-
-                            stack = new ItemStack(Material.ENDER_EYE, 1);
-                            ItemMeta meta = stack.getItemMeta();
-                            assert meta != null;
-                            meta.setDisplayName(ChatColor.DARK_PURPLE + "Show supervised tasks");
-                            stack.setItemMeta(meta);
-                            inventory.setItem(SHOW_SUPERVISED_TASKS_POSITION, stack);
-
-                            player.openInventory(inventory);
-                        });
-            } catch (SQLException ex) {
-                Bukkit.getScheduler().runTask(plugin,
-                        () -> player.sendMessage(ChatColor.RED + ex.getMessage()));
-                ex.printStackTrace();
-            }
-        });
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, ClickEventRunnables.showDashboardView(plugin, taskDAO, player));
     }
 
     private void handleArrowClick() {
