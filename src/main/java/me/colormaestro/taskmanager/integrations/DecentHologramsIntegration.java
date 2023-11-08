269fec3fd6d2ff5f5f5579dce22ce0b12907c39b
Marek Kadlečík <xkadlec5@fi.muni.cz>
2023-11-09 00:55:56 +0100

    Added javadoc to DecentHologramsIntegration

--- src/main/java/me/colormaestro/taskmanager/integrations/DecentHologramsIntegration.java
+++ src/main/java/me/colormaestro/taskmanager/integrations/DecentHologramsIntegration.java
@@ -7,11 +7,37 @@ import org.bukkit.entity.Player;
 import java.util.List;
 
 public interface DecentHologramsIntegration {
+    /**
+     * Creates hologram for player at his current position.
+     *
+     * @param player for which to create hologram
+     * @throws IllegalArgumentException if hologram for this player already exists
+     */
     void establishTasksHologram(Player player);
 
+    /**
+     * Checks whether hologram with given key exists.
+     *
+     * @param key of holograms
+     * @return true if exists, false otherwise
+     */
     boolean hologramExists(String key);
 
+    /**
+     * Teleports hologram to location. Rises height of location by 2 before teleport.
+     *
+     * @param key of hologram to teleport
+     * @param location at which to teleport the hologram
+     * @throws IllegalArgumentException if hologram with specified key does not exist
+     */
     void teleportHologram(String key, Location location);
 
+    /**
+     * Formats given tasks on hologram.
+     *
+     * @param key of hologram on which to put tasks
+     * @param tasks to put on hologram
+     * @throws IllegalArgumentException if hologram is not found
+     */
     void setTasks(String key, List<Task> tasks);
 }
