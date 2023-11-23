94cca7fd1f7555b3ac398b00737d3df9ef1440bf
Marek Kadlečík <xkadlec5@fi.muni.cz>
2023-11-23 22:59:40 +0100

    Removed unnecessary casting in NeedTasks command

--- src/main/java/me/colormaestro/taskmanager/commands/NeedTasks.java
+++ src/main/java/me/colormaestro/taskmanager/commands/NeedTasks.java
@@ -7,7 +7,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -25,7 +24,6 @@ public class NeedTasks implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        Player p = (Player) sender;
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             try {
                 int limit = args.length > 0 ? Integer.parseInt(args[0]) : 0;
@@ -34,26 +32,26 @@ public class NeedTasks implements CommandExecutor {
                         .stream()
                         .filter(memberDashboardInfo -> memberDashboardInfo.doing() <= limit)
                         .toList();
-                Bukkit.getScheduler().runTask(plugin, () -> sendMessage(p, members));
+                Bukkit.getScheduler().runTask(plugin, () -> sendMessage(sender, members));
             } catch (SQLException ex) {
-                Bukkit.getScheduler().runTask(plugin, () -> p.sendMessage(ChatColor.RED + ex.getMessage()));
+                Bukkit.getScheduler().runTask(plugin, () -> sender.sendMessage(ChatColor.RED + ex.getMessage()));
                 ex.printStackTrace();
             } catch (NumberFormatException ex) {
                 Bukkit.getScheduler().runTask(plugin,
-                        () -> p.sendMessage(ChatColor.RED + "Limit must be numerical value!"));
+                        () -> sender.sendMessage(ChatColor.RED + "Limit must be numerical value!"));
             }
         });
         return true;
     }
 
-    private void sendMessage(Player player, List<MemberDashboardInfo> members) {
+    private void sendMessage(CommandSender sender, List<MemberDashboardInfo> members) {
         if (members.isEmpty()) {
-            player.sendMessage(ChatColor.GREEN + "Everyone has enough tasks");
+            sender.sendMessage(ChatColor.GREEN + "Everyone has enough tasks");
             return;
         }
-        player.sendMessage(ChatColor.GOLD + "-=-=-=- Members running out of tasks -=-=-=-");
+        sender.sendMessage(ChatColor.GOLD + "-=-=-=- Members running out of tasks -=-=-=-");
         for (MemberDashboardInfo memberInfo : members) {
-            player.sendMessage(memberInfo.ign() + ChatColor.ITALIC + " (" + memberInfo.doing() + " tasks in progress)");
+            sender.sendMessage(memberInfo.ign() + ChatColor.ITALIC + " (" + memberInfo.doing() + " tasks in progress)");
         }
     }
 }
